bb6fc8aa5d366050bf88b6677489fe5f7e21b0ab
dan <48936611+dansvel@users.noreply.github.com>
2021-02-17 16:38:23 +0700

    add postcss-import

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,12 +2,14 @@ const postcssPresetEnv = require('postcss-preset-env');
 const tailwindCSS = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 const cssNano = require('cssnano');
+const postcssImport = require('postcss-import');
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 
 module.exports = {
     plugins: [
+        postcssImport(),
         postcssPresetEnv({
             stage: 4,
             features: {
