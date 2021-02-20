02786e72f946302a3ee5b0f6abb03a6dfd7947e9
dan <48936611+dansvel@users.noreply.github.com>
2021-02-21 00:43:14 +0700

    🐛 Fix the error route

--- src/routes/$error.svelte
+++ src/routes/$error.svelte
@@ -1,12 +1,11 @@
-<script>
-  const dev = process.env.NODE_ENV === 'development';
+<script lang="ts">
+  import {onMount} from 'svelte';
 
+  let dev
   export let status;
   export let error;
 
-  // we don't want to use <svelte:window bind:online> here,
-  // because we only care about the online state when
-  // the page first loads
+  onMount(() => dev = process.env.NODE_ENV === 'development')
   const online = typeof navigator !== 'undefined' ? navigator.onLine : true;
 </script>
 
@@ -19,7 +18,7 @@
     <h1>Oops!</h1>
 
     {#if error.message}
-      <h2>{status}</h2>
+      <h2>Galat {status}! {error.message}</h2>
     {:else}
       <h2>Encountered a {status} error</h2>
     {/if}
@@ -28,16 +27,14 @@
       <pre>{error.stack}</pre>
     {:else}
       {#if status >= 500}
-        <p>Please try reloading the page.</p>
+        <p>Tolong muat ulang halaman. Dan jika galat masih terjadi, kamu dapat <a href="https://twitter.com/dansvel" rel="noopener">mengabariku di Twitter</a> tentang ini. Terima kasih!</p>
+      {:else}
+        <p>Silakan gunakan menu di atas untuk menjelajah blog ini. Terima kasih.</p>
       {/if}
-
-      <p>
-        If the error persists, you can <a href="https://twitter.com/dansvel">contact me on Twitter</a> and let me know. Thanks!
-      </p>
     {/if}
   {:else}
-    <h1>It looks like you're offline</h1>
+    <h1>Sepertinya kamu sedang luring</h1>
 
-    <p>Reload the page once you've found the internet.</p>
+    <p>Muat ulang halaman ini ketika kamu kembali daring nanti.</p>
   {/if}
 </div>
