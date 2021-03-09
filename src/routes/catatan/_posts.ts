30dc406424944f7f8866a8cf2fc5a3ad42ae48fb
dan <48936611+dansvel@users.noreply.github.com>
2021-03-09 12:49:22 +0700

    make it smooth

--- src/routes/catatan/_posts.ts
+++ src/routes/catatan/_posts.ts
@@ -4,32 +4,26 @@ import marked from 'marked';
 import frontMatter from 'front-matter';
 import markedOptions from '../../components/marked.config';
 
-const posts = fs
-    .readdirSync('src/content/post')
-    .filter((file) => path.extname(file) === '.md')
-    .map((file) => {
-      const markdown = fs.readFileSync(path.resolve('src/content/post', file), 'utf-8');
-      const {
-        body,
-        attributes,
-      }: {
-        body: string;
-        attributes: {
-          title: string;
-          description: string;
-          tags: string[];
-          category: string;
-          image: string;
-          date: string;
-        };
-      } = frontMatter(markdown);
-      return {
-        ...attributes,
-        slug: path.basename(file, '.md'),
-        body: marked(body, markedOptions),
-      };
-    });
-
 export function getPost(slug: string) {
-  return posts.find((posts, i) => posts.slug === slug);
+  const markdown = fs.readFileSync(path.resolve('src/content/post', `${slug}.md`), 'utf-8');
+
+  const {
+    body,
+    attributes,
+  }: {
+    body: string;
+    attributes: {
+      title: string;
+      description: string;
+      tags: string[];
+      category: string;
+      image: string;
+      date: string;
+    };
+  } = frontMatter(markdown);
+  return {
+    ...attributes,
+    slug,
+    body: marked(body, markedOptions),
+  };
 }
--- src/setup/index.ts
+++ src/setup/index.ts
@@ -2,34 +2,13 @@ import fs from 'fs/promises';
 import path from 'path';
 // @ts-ignore
 import { dev } from '$app/env';
-import marked from 'marked';
 import frontMatter from 'front-matter';
-import markedOptions from "$components/marked.config";
-
-// const getPost = async (slug) => {
-//   if (dev) {
-//     // @ts-ignore
-//     const { body, markdown, ...meta } = await import(`../../_content/post/${slug}.js`).then(module => module.default);
-//     return {...meta}
-//   } else {
-//     // @ts-ignore
-//     const { body, markdown, ...meta } = await import(`../../../unoptimized/client/_content/post/${slug}.js`).then(module => module.default);
-//     return { ...meta }
-//   }
-// }
 
 export async function getSession(): Promise<Record<string, any>> {
   const filenames = await fs.readdir('src/content/post');
   const pages = await Promise.all(
     filenames.map(async (filename) => {
-      // const slug = await path.basename(filename, '.md');
-      // const post = await getPost(slug).then(data => data)
       const post = await fs.readFile(path.resolve('src/content/post', filename), 'utf-8');
-
-      // return {
-      //   ...post,
-      //   slug
-      // };
 
       const {
         attributes,
@@ -49,6 +28,6 @@ export async function getSession(): Promise<Record<string, any>> {
       };
     })
   );
-  // console.log(pages, ' => pages')
+
   return { pages };
 }
