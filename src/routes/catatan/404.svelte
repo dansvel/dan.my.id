0d02e14f2f73e5b596fe01562a022b888ea338ac
dan <48936611+dansvel@users.noreply.github.com>
2021-03-18 13:09:57 +0700

    netlify now can handle 404

---
+++ src/routes/catatan/404.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import '../_global.postcss';
+  import Transition from '$lib/components/Transition.svelte';
+  import SeoHead from '$lib/components/SeoHead.svelte';
+</script>
+
+<SeoHead title="Oops, catatan seperti itu tidak ada - Blog milik Dan" />
+
+<Transition key="Oops, catatan seperti itu tidak ada - Blog milik Dan">
+  <div class="prose lg:prose-xl max-w-none">
+    <h1>Oops!</h1>
+    <h2>Catatan seperti itu tidak ada</h2>
+
+    <p>Silakan gunakan menu di atas untuk menjelajah blog ini.</p>
+  </div>
+</Transition>
+
+<style lang="postcss">
+  div {
+    @apply text-center;
+    & pre {
+      @apply text-left;
+    }
+  }
+</style>
