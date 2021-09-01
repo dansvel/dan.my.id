0f524d0a89d0d9532ae24d627781e09b56a21514
dan <dansvel@protonmail.com>
2021-09-01 23:30:29 +0700

    fix error page

---
+++ src/routes/404.svelte
@@ -0,0 +1,18 @@
+
+<script>
+  import SeoHead from '$lib/SeoHead.svelte';
+</script>
+
+<SeoHead
+        title="dan.my.id - 404: Halaman tidak ditemukan"
+/>
+
+<div>
+  <article>
+    <header>
+      <h1>Oops! Halaman tidak ditemukan</h1>
+    </header>
+    <p>Sepertinya kamu tersesat, mari aku tunjukkan jalan <a href="/">kembali</a>.</p>
+    <p>Atau kamu dapat menggunakan menu di atas untuk navigasi di blog ini.</p>
+  </article>
+</div>
