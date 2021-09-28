c52715007461b435526bc70eecf3bc7c82f7a6c9
dansvel <dansvel@protonmail.com>
2021-09-29 00:44:21 +0700

    nghapus comment

--- src/lib/CatatanList.svelte
+++ src/lib/CatatanList.svelte
@@ -11,20 +11,15 @@
   {/if}
   {#each notes as note}
     <article>
-<!--      <div class="thumbnail">-->
-<!--        <img src={note.image} alt="Thumbnail for {note.title}" />-->
-<!--      </div>-->
-<!--      <div class="note">-->
-        <h3><a sveltekit:prefetch href="/catatan/{note.slug}">{note.title}</a></h3>
-        <small>{localDate(note.date)}</small>
-        <div>
-          {#each note.tags as tag}
-            <a sveltekit:prefetch href="/catatan/?label={slugger(tag)}">#{tag}</a>
-            <span class="last:hidden">&nbsp;</span>
-          {/each}
-        </div>
-        <p>{note.description}</p>
-<!--      </div>-->
+      <h3><a sveltekit:prefetch href="/catatan/{note.slug}">{note.title}</a></h3>
+      <small>{localDate(note.date)}</small>
+      <div>
+        {#each note.tags as tag}
+          <a sveltekit:prefetch href="/catatan/?label={slugger(tag)}">#{tag}</a>
+          <span class="last:hidden">&nbsp;</span>
+        {/each}
+      </div>
+      <p>{note.description}</p>
     </article>
   {:else}
     <blockquote>Oops, belum ada catatan tentang itu</blockquote>
@@ -39,21 +34,20 @@
     }
     article {
       @apply py-4 h-full;
-        h3 {
-          @apply mt-2 md:mt-0 mb-2 border-0;
-          a {
-            @apply border-b hover:(border-b-3)
+      h3 {
+        @apply mt-2 md:mt-0 mb-2 border-0;
+        a {
+          @apply border-b hover:(border-b-3)
             text-indigo-700 dark:text-indigo-300
             visible m-0;
-          }
         }
-        small {
-          @apply text-gray-500;
-        }
-        p {
-          @apply mt-2 mb-0;
-        }
-
+      }
+      small {
+        @apply text-gray-500;
+      }
+      p {
+        @apply mt-2 mb-0;
+      }
     }
   }
 </style>
