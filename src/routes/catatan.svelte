49d1e71e0a03d2ff574668c2debea56bb9095913
dansvel <dansvel@protonmail.com>
2021-11-08 21:35:05 +0700

    fix filter in route false

--- src/routes/catatan.svelte
+++ src/routes/catatan.svelte
@@ -4,21 +4,20 @@
 	import CatatanList from '$lib/CatatanList.svelte';
 	import { get } from 'svelte/store';
 	import SeoHead from '$lib/SeoHead.svelte';
-	import { browser } from '$app/env';
+	import { browser, dev } from '$app/env';
 
-	let allPosts;
+	let allPosts
 	const allTags = get(session).tags;
 
-	let notes, navurl, more, pageNum;
+	let notes, navurl, more
+	let pageNum = 1;
 	let filter
 	const per = 9;
 
 
 
 	$: {
-		if (browser) {
-			filter = getUrlParams(window.location.search)
-		}
+			filter = dev ? getUrlParams($page.path.params) :  browser && getUrlParams(window.location.search)
 
 		if (filter.label) {
 			allPosts = get(session).notes.filter((post) =>
@@ -29,12 +28,15 @@
 		} else {
 			allPosts = get(session).notes;
 		}
-		pageNum = parseInt(filter?.hal || 1);
-		delete filter.hal;
+		if (filter.hal) {
+			pageNum = parseInt(filter.hal);
+			delete filter.hal;
+		}
 		more = allPosts.length - pageNum * per <= 0;
 		notes = allPosts.slice(pageNum * per - per, pageNum * per);
 
 		navurl = '?' + urlParamsToQuery(filter) + (Object.keys(filter).length ? '&' : '') + 'hal=';
+		// navurl = '?hal=';
 	}
 </script>
 
@@ -48,8 +50,8 @@
 		Catatan
 		{#if JSON.stringify(filter) !== '{}'}
 			tentang
-			{filter.label ? get(session).tags.filter((slug) => slugger(slug) === filter.label) : ''}
-			{filter.kategori ? capitalize(filter?.kategori) : ''}
+			<!--{filter.label ? get(session).tags.filter((slug) => slugger(slug) === filter.label) : ''}-->
+			<!--{filter.kategori ? capitalize(filter?.kategori) : ''}-->
 		{/if}
 	</h1>
 	<div class="text-center">
