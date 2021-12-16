367b918ffbdfb6e357e532ac19295cb00d9ab834
dansvel <dansvel@protonmail.com>
2021-12-16 16:11:35 +0700

    remove unuse code

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,15 +2,6 @@
   import 'virtual:windi.css'
   import './app.postcss'
   import Nav from '$lib/Nav.svelte'
-  // import { onMount } from 'svelte'
-
-  // onMount(async () => {
-  //   if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  //     document.querySelector('link[title=Dark]').removeAttribute('disabled')
-  //   } else {
-  //     document.querySelector('link[title=Light]').removeAttribute('disabled')
-  //   }
-  // })
 </script>
 
 <Nav />
