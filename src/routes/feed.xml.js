b008ba9da5e5f7ceb7f7c1a3b0d9319cf95d600f
dansvel <dansvel@protonmail.com>
2022-02-24 17:21:09 +0700

    fix feed

--- src/routes/feed.xml.js
+++ src/routes/feed.xml.js
@@ -1,26 +1,31 @@
 import RSS from 'rss'
+import { siteTitle, siteDescription, siteURL } from "$lib/config.js";
 
 export const get = () => {
   const feed = new RSS({
-    title: 'Blog milik Dan',
-    description: 'Catatan perjalanan Setengah Manusia',
-    site_url: 'htps://dan.my.id',
+    title: siteTitle,
+    description: siteDescription,
+    site_url: siteURL,
     language: 'id'
   })
 
   const files = import.meta.globEager('../content/posts/*.md')
   for (const path in files) {
-    const {
-      attributes: { title, description, date }
-    } = files[path]
+    if (path.split('/').pop().match(/\.md$/)) {
+      const { metadata } = files[path]
 
-    feed.item({
-      title,
-      description,
-      url: 'https://dan.my.id/' + path.split('/').pop().split('.').shift(),
-      date
-    })
+      if (!metadata.draft) {
+        feed.item({
+          title: metadata.title,
+          description: metadata.description,
+          url: siteURL + path.split('/').pop().slice(0, -3),
+          date: metadata.date
+        })
+      }
+
+    }
   }
+
 
   return {
     headers: { 'content-type': 'application/xml' },
