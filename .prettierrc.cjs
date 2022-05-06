394eb7d7c829d790bf02dcd1a7a046fc7c2b6cdd
dansvel <dansvel@protonmail.com>
2022-05-06 18:29:02 +0700

    fresh start

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+const prettierSvelte = require('prettier-plugin-svelte')
+
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  plugins: [prettierSvelte],
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  svelteIndentScriptAndStyle: true,
+  svelteSortOrder: 'options-scripts-markup-styles',
+  svelteStrictMode: false,
+  tabWidth: 2,
+}
