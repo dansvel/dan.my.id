723a771cd6b39d15e1835aa12a77a3edf6c0a9f4
dansvel <dansvel@protonmail.com>
2022-05-21 19:52:50 +0700

    fix nav animation

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,27 +1,9 @@
 <script>
   import config from '$lib/config.yaml'
   const { title, textLogo, navItems, socialLinks } = config
-
-  let show = true
-  let last_scroll = 0
-  let hash_changed = false
-  function handle_hashchange() {
-    hash_changed = true
-  }
-
-  function handle_scroll() {
-    const scroll = window.pageYOffset
-    if (!hash_changed) {
-      show = scroll < 500 || scroll < last_scroll
-    }
-
-    last_scroll = scroll
-    hash_changed = false
-  }
 </script>
 
-<svelte:window on:hashchange={handle_hashchange} on:scroll={handle_scroll} />
-<header class:show>
+<header>
   <nav>
     <div id="brand">
       <a href="/" {title} sveltekit:prefetch>{textLogo}</a>
@@ -49,8 +31,8 @@
 
 <!-- just for static adapter -->
 <div class="hidden">
-    <a href="/404">404</a>
-    <a href="/sitemap.xml">Sitemap</a>
+  <a href="/404">404</a>
+  <a href="/sitemap.xml">Sitemap</a>
   <a href="/feed.xml">RSS feed</a>
 </div>
 
@@ -59,10 +41,7 @@
     @apply py-4 sticky z-99 top-0;
     @apply shadow-b shadow-md shadow-light-500 bg-light-500 dark:(shadow-dark-500 bg-dark-500);
 
-
     nav {
-
-
       > div {
         @apply flex basis-full;
         &#brand {
